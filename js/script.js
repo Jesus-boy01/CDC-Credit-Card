4f7c5b83d29d28554d4a02394154a366a0b7cabd
Tolulope Dada <tdada0912@gmail.com>
2024-05-09 19:15:33 +0100

    designed a credit card

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let cardNumber = document.getElementById("card-number");
+let paymentNetworkLogo = document.querySelector(".payment-network-logo");
+
+cardNumber.addEventListener('input', cardNumberFunction);
+
+function cardNumberFunction() {
+    for (let count = 0; count < cardNumber.value.length; count++) {
+        if(cardNumber.value < 4 || cardNumber > 5){
+            alert("Invalid Card Number");
+            return;
+        }
+
+        if((cardNumber.value[0] === "5") && ((cardNumber.value.substring(0, 4)) !== "5061")){
+            masterPaymentNetwork();
+        }
+
+        if((cardNumber.value[0] === "5") && ((cardNumber.value.substring(0, 4)) === "5061")){
+            vervePaymentNetwork();
+        }
+        
+        if((cardNumber.value[0] === "4")){
+            visaPaymentNetwork();
+        }
+    }
+}
+
+function masterPaymentNetwork() {
+    let handler = ""
+
+    handler = `<img src="img/mastercard.png" alt="Master Card" id="payment-network-logo">`;
+
+    paymentNetworkLogo.innerHTML = handler;
+}
+
+function vervePaymentNetwork() {
+    let handler = ""
+
+    handler = `<img src="img/verve.png" alt="Verve Card" id="payment-network-logo">`;
+
+    paymentNetworkLogo.innerHTML = handler;
+}
+
+function visaPaymentNetwork() {
+    let handler = ""
+
+    handler = `<img src="img/visa.png" alt="Visa Card" id="payment-network-logo">`;
+
+    paymentNetworkLogo.innerHTML = handler;
+}
+
+function clearInput(e) {
+    e.preventDefault();
+
+    cardNumber.reset();
+}
